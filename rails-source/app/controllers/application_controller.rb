e96e02c208da91698dbff652c0876dde9d2dd8c4
seigo <seigoto23@gmail.com>
2018-12-05 16:08:32 +0900

    なぜかapiモードの設定が生きてたので修正

--- rails-source/app/controllers/application_controller.rb
+++ rails-source/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+
+  p "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  p "#{ENV.fetch("DB_NAME") { database_hoge }}"
+
 end
